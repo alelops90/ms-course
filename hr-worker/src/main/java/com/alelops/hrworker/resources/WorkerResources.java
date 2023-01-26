32be36cacb39fafae19bbabeaa9882907f4d955c
alexandre <alexandre.lopes@efabroka.com.br>
2023-01-26 14:27:11 -0300

    Ribbon and EurekaServer

--- hr-worker/src/main/java/com/alelops/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/com/alelops/hrworker/resources/WorkerResources.java
@@ -2,7 +2,10 @@ package com.alelops.hrworker.resources;
 
 import com.alelops.hrworker.entites.Worker;
 import com.alelops.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,11 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerResources {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerResources.class);
+
+    @Autowired
+    private Environment environment;
+
     @Autowired
     private WorkerRepository repository;
 
@@ -26,6 +34,7 @@ public class WorkerResources {
 
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+        logger.info("port = " + environment.getProperty("local.server.port"));
         Worker worker = repository.findById(id).get();
         return ResponseEntity.ok(worker);
     }

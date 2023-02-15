d009a267c58db946f02ff9ab3bc458baef0204ff
alexandre <alexandre.lopes@efabroka.com.br>
2023-02-15 15:21:08 -0300

    Zull Authorizaton

--- hr-oauth/src/main/java/com/alelops/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/alelops/hroauth/services/UserService.java
@@ -4,11 +4,14 @@ import com.alelops.hroauth.entities.User;
 import com.alelops.hroauth.feignclient.UserFeignClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserFeignClient userFeignClient;
@@ -18,6 +21,17 @@ public class UserService {
         if (user == null) {
             log.error("Not Found " + email);
             throw new IllegalArgumentException("Not Found");
+        }
+        log.info("Passou por aqui essa caraia..."+ email);
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userFeignClient.findByEmail(email).getBody();
+        if (user == null) {
+            log.error("Not Found " + email);
+            throw new UsernameNotFoundException("Not Found");
         }
         log.info("Passou por aqui essa caraia..."+ email);
         return user;
